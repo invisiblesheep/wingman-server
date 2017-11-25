4faab1b421a39eba0ca3f73aaf4d68da7f0fa61a
Tuomas Jokioja <tuomas.jokioja@bcbmedical.com>
2017-11-25 11:03:21 +0200

    Add schema for category, add db functions and initial idea for using categories to fire up different ml-models

--- Wing.model.js
+++ Wing.model.js
@@ -1,8 +1,21 @@
+import mongoose from 'mongoose';
+import parse from 'co-body';
+import { exec } from 'child_process';
+
+const Category = mongoose.model('Category', new mongoose.Schema({
+    name: {
+        type: String,
+        enum: ['weather', 'social_media', 'everything_else']
+    },
+    subcategories: [String]
+
+}));
+
 const Wing = mongoose.model('Wing', new mongoose.Schema({
     // wingId: Number,
     heading: String,
     story: String,
-    categories: Array,
+    categories: [Category],
     impact: {
         type: Number,
         min: 0,
@@ -12,10 +25,48 @@ const Wing = mongoose.model('Wing', new mongoose.Schema({
         type: String,
         enum: ['scraper1', 'scraper2', 'scraper3']
     },
-    received: Date,
+    received: { type: Date, default: Date.now },
     processed: Date,
     status: {
         type: String,
         enum: ['accepted', 'declined', 'waiting']
     }
 }));
+
+
+
+export async function getWings(ctx) {
+    const results = await Bulletin.find();
+    ctx.body = JSON.stringify(results);
+}
+
+export async function postWing(ctx) {
+    const parsed = await parse(ctx);
+    parsed.status = 'waiting';
+
+    //example of using different models for different categories..
+    if (parsed.categories.includes('weather')){
+        exec(`python weatherModel.py "${parsed.text}"`, (err, stdout, stderr) => {
+            if (err) {
+                console.log(err, 'errors');
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+            parsed.impact = stdout;
+        });
+    }
+
+
+    // console.log(parsed);
+    // newsworth from python world
+    //parsed.push({rating:{credibility, }})
+    await Wing.create(parsed);
+    ctx.body = {data: 'very niceee'};
+}
+
+export async function processWing(ctx) {
+    const parsed = await parse(ctx);
+
+    await Wing.update({_id: parsed.id}, { $set: {processed: parsed.processed}})
+}
